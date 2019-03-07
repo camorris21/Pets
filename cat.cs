11307df713f871356f42fb5a26df425a5ff58199
Chelsea Morris <42583050+camorris21@users.noreply.github.com>
2019-03-06 22:03:19 -0600

    Add files via upload
    screwed up and couldn't make commits via terminal. accidentally did code on master branch not feature. don't do again.

---
+++ cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pets
+{
+    class Cat : Pet
+    {
+        public Cat(string petName, string ownerName, double petWeight) : base("Cat", petName, ownerName, petWeight)
+        {
+            name = petName;
+            owner = ownerName;
+            weight = petWeight;
+        }
+
+        public string meow(int count)
+        {
+            string talk = null;
+            int i;
+            for (i = 0; i < count; i++)
+                talk += "meow...";
+            return talk;
+        }
+    }
+}
